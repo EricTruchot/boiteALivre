3d59054576bd4b17ef7cdda24af1241ba474dca2
EricTruchot <eric.truchot12@gmail.com>
2023-05-16 15:47:59 +0200

    feat(routing):add routing system

---
+++ index.js
@@ -0,0 +1,9 @@
+import { ExpoRoot } from "expo-router";
+import { registerRootComponent } from 'expo';
+
+export default function App() {
+    const ctx = require.context("./src/app/");
+    return <ExpoRoot context={ctx} />;
+}
+
+registerRootComponent(App);
--- src/app/contact/index.js
+++ src/app/contact/index.js
@@ -1,14 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function Page() {
   return (
     <View style={styles.container}>
       <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+        <Text style={styles.title}>#contact</Text>
       </View>
-      <Link href="/contact">Go to your profile</Link>
     </View>
   );
 }
@@ -18,16 +16,22 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     padding: 24,
-  },
-  main: {
-    flex: 1,
     justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
+    backgroundColor: "black",
   },
+//   main: {
+//     flex: 1,
+//     justifyContent: "center",
+//     // maxWidth: 960,
+//     margin: 0,
+//     backgroundColor: "black",
+//     width: 100,
+
+//   },
   title: {
     fontSize: 64,
     fontWeight: "bold",
+    color: "white",
   },
   subtitle: {
     fontSize: 36,
